4f346a7a6936826fd81fc9948ac68289f5f0a203
Kovith <kovithkovi123@gmail.com>
2024-07-09 22:05:31 +0530

    Update UI

--- jobReco/jobReco/views.py
+++ jobReco/jobReco/views.py
@@ -33,10 +33,6 @@ def extract_text_from_pdf(file_path):
 
 
 def index(request):
-    return render(request, 'index.html')
-
-
-def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['resume']
         # Save the uploaded file to the media directory
@@ -51,45 +47,25 @@ def upload(request):
         elif uploaded_file.name.endswith('.pdf'):
             resume_text = extract_text_from_pdf(file_path)
         else:
-            # Delete the file if the format is unsupported
             fs.delete(filename)
             return HttpResponse("Unsupported file format. Please upload a .docx or .pdf file.")
-
         cleaned_resume = clean_resume(resume_text)
-
-        # Print the first 500 characters of the cleaned resume
         print(cleaned_resume[:500])
-
-        # Load dataset and fit vectorizer
         df = pd.read_csv(
             'C:/records/web development/Intervita/djangoEnv/jobReco/jobReco/UpdatedResumeDataSet111.csv')
-
-        # Vectorize resume text
         vectorizer = TfidfVectorizer(max_features=5000)
         vectorizer.fit(df['Resume'])
-
         resume_vector = vectorizer.transform([cleaned_resume])
-
-        # Load the trained model
         with open('C:/records/web development/Intervita/djangoEnv/jobReco/jobReco/job_recommendation_model.pkl', 'rb') as model_file:
             model = pickle.load(model_file)
-
-        # Predict categories and recommend jobs
         predicted_probabilities = model.predict_proba(resume_vector)
-
-        # Assuming `job_listings` is a DataFrame containing job listings with a 'Category' column
-        # Assuming `categories` are the possible job categories in your dataset
         categories = model.classes_
         top_categories = np.argsort(predicted_probabilities[0])[
-            ::-1][:5]  # Top 5 predicted categories
-
+            ::-1][:5]
         print("Top Predicted Categories:")
         for idx in top_categories:
             print(f"{categories[idx]}")
-
-        # Delete the file after processing
         fs.delete(filename)
-
         return render(request, 'upload.html', {'top_categories': [categories[idx] for idx in top_categories]})
 
-    return render(request, 'upload.html')
+    return render(request, 'index.html')
